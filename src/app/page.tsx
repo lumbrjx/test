e3a086faa9c4d871618e1a87a2d2774e0e413abd
lumbrjx <kaibitayeb01@gmail.com>
2024-07-07 14:28:06 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,70 +2,83 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [state, setState] = useState("");
-  const [state2, setState2] = useState(false);
-  function applyChedah(str: string) {
-    let d = str.split(" ");
+	const [state, setState] = useState("");
+	const [state2, setState2] = useState(false);
+	const chemsya = [
+		"ت",
+		"ث",
+		"د",
+		"ذ",
+		"ر",
+		"ز",
+		"س",
+		"ش",
+		"ص",
+		"ض",
+		"ط",
+		"ظ",
+		"ل",
+		"ن",
+	];
 
-    let rs: string[] = [];
-    d.forEach((e) => {
-      if (e.startsWith("ال") === true) {
-        const chemsya = [
-          "ت",
-          "ث",
-          "د",
-          "ذ",
-          "ر",
-          "ز",
-          "س",
-          "ش",
-          "ص",
-          "ض",
-          "ط",
-          "ظ",
-          "ل",
-          "ن",
-        ];
-        const l = chemsya.includes(e[2]);
-        if (l === true) {
-          e = e.slice(0, 3) + "\u0651" + e.slice(3);
+	function swap(word: string, range: number, push_list: string[]) {
+		const l = chemsya.includes(word[range]);
+		if (l === true) {
+			word = word.slice(0, range + 1) + "\u0651" + word.slice(range + 1);
+			push_list.push(word);
+		} else {
+			push_list.push(word);
+		}
 
-          rs.push(e);
-        } else {
-          rs.push(e);
-        }
-      } else {
-        rs.push(e);
-      }
-    });
-    setState(rs.join(" "));
-  }
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex  flex-col items-center gap-7">
-        <p>text?</p>
-        <textarea
-          onChange={(e) => {
-            setState(e.target.value);
-            setState2(false);
-          }}
-          className="text-black"
-        />
-        <button
-          className="text-blue-500"
-          onClick={() => {
-            setState2(true);
-            applyChedah(state);
-          }}
-        >
-          Validate
-        </button>
-      </div>
-      {state2 === true && (
-        <div className="flex  flex-col items-center gap-7">
-          <p>{state}</p>
-        </div>
-      )}
-    </main>
-  );
+	}
+	function applyChedah(str: string) {
+		let d = str.split(" ");
+		let rs: string[] = [];
+		d.forEach((e) => {
+			// ladi, lati, ina, ana cases
+			if (e === "الذي" || e === "التي" || e === "إن" || e === "أن") {
+				e = e.slice(0, 2) + "\u0651" + e.slice(2);
+				rs.push(e);
+				return
+			}
+
+			if (e.startsWith("ال") === true) {
+				swap(e, 2, rs)
+			} else if (e.startsWith("وال") === true || e.startsWith("فال") === true) {
+				swap(e, 3, rs)
+			}
+			else {
+				rs.push(e);
+			}
+		});
+		setState(rs.join(" "));
+	}
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="flex  flex-col items-center gap-7">
+				<p>text?</p>
+				<textarea
+					onChange={(e) => {
+						setState(e.target.value);
+						setState2(false);
+					}}
+					className="text-black"
+				/>
+				<button
+					className="text-blue-500"
+					onClick={() => {
+						setState2(true);
+						applyChedah(state);
+					}}
+				>
+					Validate
+				</button>
+			</div>
+			{state2 === true && (
+				<div className="flex  flex-col items-center gap-7">
+					<p>{state}</p>
+				</div>
+			)}
+		</main>
+	);
 }
